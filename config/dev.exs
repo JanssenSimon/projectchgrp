c638b55196ace56c33f09562b7bae4444f3335ea
JanssenSimon <32472583+JanssenSimon@users.noreply.github.com>
2024-01-15 16:37:24 -0500

    Enables phoenix container to connect to postgres container through docker network

--- config/dev.exs
+++ config/dev.exs
@@ -4,7 +4,7 @@ import Config
 config :monaserver, Monaserver.Repo,
   username: "postgres",
   password: "postgres",
-  hostname: "localhost",
+  hostname: "postgres",
   database: "monaserver_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
