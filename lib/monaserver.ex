b156dfa60e0e855a00e19466330c9a527b3cd7d4
JanssenSimon <32472583+JanssenSimon@users.noreply.github.com>
2024-01-15 13:21:44 -0500

    Initial commit

---
+++ lib/monaserver.ex
@@ -0,0 +1,18 @@
+defmodule Monaserver do
+  @moduledoc """
+  Documentation for `Monaserver`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Monaserver.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/monaserver_test.exs
@@ -0,0 +1,8 @@
+defmodule MonaserverTest do
+  use ExUnit.Case
+  doctest Monaserver
+
+  test "greets the world" do
+    assert Monaserver.hello() == :world
+  end
+end
